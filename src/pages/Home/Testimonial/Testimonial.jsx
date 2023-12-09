053a5f2f77826dbcf00ee637c7c66db5b69c5976
joy-mahmud <joymahmudiucse@gmail.com>
2023-12-10 01:28:46 +0600

    live server link added

--- src/pages/Home/Testimonial/Testimonial.jsx
+++ src/pages/Home/Testimonial/Testimonial.jsx
@@ -12,7 +12,7 @@ import 'swiper/css/navigation';
 const Testimonial = () => {
     const [reviews, setReviews] = useState([])
     useEffect(() => {
-        fetch('http://localhost:5000/reviews')
+        fetch('https://bistro-boss-server-ebon.vercel.app/reviews')
             .then(res => res.json())
             .then(data => setReviews(data))
     }, [])
--- src/routes/Route/Route.jsx
+++ src/routes/Route/Route.jsx
@@ -83,7 +83,7 @@ export const router = createBrowserRouter([
         {
           path:'updateItem/:id',
           element:<AdminRoute><UpdateItem></UpdateItem></AdminRoute>,
-          loader:({params})=>fetch(`http://localhost:5000/menu/${params.id}`)
+          loader:({params})=>fetch(`https://bistro-boss-server-ebon.vercel.app/menu/${params.id}`)
         },
         {
           path:'allUsers',
